b4106f86db781d4b1d73e1f697329034fa2cf29d
Owen Glahn <owen.glahn@mail.mcgill.ca>
2022-09-22 17:07:45 -0400

    Renamed to move_robot :

--- cubic_polynomial_planner/src/server.cpp
+++ cubic_polynomial_planner/src/server.cpp
@@ -1,43 +1,27 @@
 #include <ros/ros.h>
 #include <std_msgs/String.h>
-#include "cubic_polynomial_planner/my_service.h"
+// #include "cubic_polynomial_planner/move_robot.h"
 
-class Server {
-public:
-	Server() {
-		// advertise service	
-		this.service = node_handle
-			.advertiseService("my_service", &Server::call_back, this);	
-	}
+// class Server {
+// public:
+// 	Server() {
+// 		// advertise service	
+// 		this.service = node_handle
+// 			.advertiseService("my_service", &Server::call_back, this);	
+// 	}
 
-	bool call_back(cubic_polynomial_planner::my_service::Request &req, 
-					cubic_polynomial_planner::my_service::Response &res) {
-		ROS_INFO("Server: x=%ld, y=%ld, z=%ld, T=%ld", (long int) req.x, (long int) req.y, 
-			(long int) req.z, (long int) req.T);
-		this.x = (long int) req.x;
-		this.y = (long int) req.y;
-		this.z = (long int) req.x;
-		this.T = (long int) req.T;
-		res.result = "";
-		return true;
-	}
+// 	bool call_back(cubic_polynomial_planner::move_robot::Request &req, 
+// 					cubic_polynomial_planner::move_robot::Response &res) {
+// 		ROS_INFO("Server: x=%ld, y=%ld, z=%ld, T=%ld", (long int) req.x, (long int) req.y, 
+// 			(long int) req.z, (long int) req.T);
+// 		res.result = "";
+// 		return true;
+// 	}
 
-	int[] getPosition() {
-		int toReturn[4];
-		toReturn[0] = this.x;
-		toReturn[1] = this.y;
-		toReturn[2] = this.z;
-		toReturn[3] = this.T;
-		return toReturn;
-	}
-private:
-	ros::NodeHandle node_handle;
-	ros::ServiceServer service; 
-	long int x;
-	long int y;
-	long int z;
-	long int T;	
-};
+// private:
+// 	ros::NodeHandle node_handle;
+// 	ros::ServiceServer service; 
+// };
 
 int main(int argc, char **argv) {
 
@@ -45,7 +29,7 @@ int main(int argc, char **argv) {
 	ros::init(argc, argv, "publisher_node") ;
 	
 	// create server
-	Server server;
+	// Server server;
 
 	// specify the frequency to 10HZ
 	ros::Rate loopRate(10) ;
@@ -58,7 +42,6 @@ int main(int argc, char **argv) {
 
 		// sleep for any time remaining to the publish rate
 		loopRate.sleep() ;
-		counter++ ;
 	}
 	return 0 ;
 }
