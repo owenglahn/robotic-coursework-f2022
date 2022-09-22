d32ac40d3d680a44701ab6ce862889b7cdf7d422
Owen Glahn <owen.glahn@mail.mcgill.ca>
2022-09-22 16:00:53 -0400

    Server class

--- cubic_polynomial_planner/src/server.cpp
+++ cubic_polynomial_planner/src/server.cpp
@@ -2,46 +2,59 @@
 #include <std_msgs/String.h>
 #include "cubic_polynomial_planner/my_service.h"
 
-bool call_back(cubic_polynomial_planner::my_service::Request &req, 
-				cubic_polynomial_planner::my_service::Response &res) {
-	ROS_INFO("Server: x=%ld, y=%ld, z=%ld, T=%ld", (long int) req.x, (long int) req.y, 
-		(long int) req.z, (long int) req.T);
-}
+class Server {
+public:
+	Server() {
+		// advertise service	
+		this.service = node_handle
+			.advertiseService("my_service", &Server::call_back, this);	
+	}
+
+	bool call_back(cubic_polynomial_planner::my_service::Request &req, 
+					cubic_polynomial_planner::my_service::Response &res) {
+		ROS_INFO("Server: x=%ld, y=%ld, z=%ld, T=%ld", (long int) req.x, (long int) req.y, 
+			(long int) req.z, (long int) req.T);
+		this.x = (long int) req.x;
+		this.y = (long int) req.y;
+		this.z = (long int) req.x;
+		this.T = (long int) req.T;
+		res.result = "";
+		return true;
+	}
+
+	int[] getPosition() {
+		int toReturn[4];
+		toReturn[0] = this.x;
+		toReturn[1] = this.y;
+		toReturn[2] = this.z;
+		toReturn[3] = this.T;
+		return toReturn;
+	}
+private:
+	ros::NodeHandle node_handle;
+	ros::ServiceServer service; 
+	long int x;
+	long int y;
+	long int z;
+	long int T;	
+};
 
 int main(int argc, char **argv) {
 
 	// initialize ROS
 	ros::init(argc, argv, "publisher_node") ;
-
-	// create a node handle
-	ros::NodeHandle node_handle ;
-
-	// advertise the topic "my_topic" 
-	ros::Publisher publisher = node_handle.advertise<std_msgs::String>("my_topic", 1) ;
+	
+	// create server
+	Server server;
 
 	// specify the frequency to 10HZ
 	ros::Rate loopRate(10) ;
 
-	// make a string message
-	std_msgs::String message ;
-
-	int	counter = 0 ;
-
 	// ros::ok() returns false when the program is finished (e.g., when you do Ctrl-C)
-	while ( ros::ok() ) {	
-
-		// change the content of the message 
-		message.data = "hello world " + std::to_string(counter) ;
-
-		// output the message to the screen
-		ROS_INFO_STREAM("publisher: " << message.data) ;
-
-		// publish the message
-		publisher.publish(message) ;
-
+	while ( ros::ok() ) {
 		// call all the callbacks waiting to be called 
 		// ( in this case, nothing )
-		ros::spinOnce() ;
+		ros::spin() ;
 
 		// sleep for any time remaining to the publish rate
 		loopRate.sleep() ;
