f131b76a348c68362ac9ad08d6b3be09fa1cfe71
Owen Glahn <owen.glahn@mail.mcgill.ca>
2022-09-23 14:52:22 -0400

    cubic polynomial planner builds. gazebo build failing

--- cubic_polynomial_planner/src/server.cpp
+++ cubic_polynomial_planner/src/server.cpp
@@ -1,27 +1,33 @@
 #include <ros/ros.h>
 #include <std_msgs/String.h>
-// #include "cubic_polynomial_planner/move_robot.h"
+#include "cubic_polynomial_planner/move_robot.h"
 
-// class Server {
-// public:
-// 	Server() {
-// 		// advertise service	
-// 		this.service = node_handle
-// 			.advertiseService("my_service", &Server::call_back, this);	
-// 	}
+class Server {
+public:
+	Server() {
+		// advertise service	
+		this -> service = node_handle
+			.advertiseService("my_service", &Server::call_back, this);	
+		this -> ros_start_time = ros::Time::now();
+	}
 
-// 	bool call_back(cubic_polynomial_planner::move_robot::Request &req, 
-// 					cubic_polynomial_planner::move_robot::Response &res) {
-// 		ROS_INFO("Server: x=%ld, y=%ld, z=%ld, T=%ld", (long int) req.x, (long int) req.y, 
-// 			(long int) req.z, (long int) req.T);
-// 		res.result = "";
-// 		return true;
-// 	}
+	bool call_back(cubic_polynomial_planner::move_robot::Request &req, 
+					cubic_polynomial_planner::move_robot::Response &res) {
+		ROS_INFO("Server: x=%ld, y=%ld, z=%ld, T=%ld", (long int) req.x, (long int) req.y, 
+			(long int) req.z, (long int) req.T);
+		this -> duration = ros::Time::now() - this -> ros_start_time;
+		res.result = "";
+		this -> current_time = ros::Time::now().toSec();
+		return true;
+	}
 
-// private:
-// 	ros::NodeHandle node_handle;
-// 	ros::ServiceServer service; 
-// };
+ private:
+ 	ros::NodeHandle node_handle;
+ 	ros::ServiceServer service;
+	ros::Duration ros_start_time;
+	double current_time;
+	ros::Duration duration;
+ };
 
 int main(int argc, char **argv) {
 
@@ -29,7 +35,7 @@ int main(int argc, char **argv) {
 	ros::init(argc, argv, "publisher_node") ;
 	
 	// create server
-	// Server server;
+	Server server;
 
 	// specify the frequency to 10HZ
 	ros::Rate loopRate(10) ;
@@ -38,7 +44,7 @@ int main(int argc, char **argv) {
 	while ( ros::ok() ) {
 		// call all the callbacks waiting to be called 
 		// ( in this case, nothing )
-		ros::spin() ;
+		ros::spinOnce() ;
 
 		// sleep for any time remaining to the publish rate
 		loopRate.sleep() ;
